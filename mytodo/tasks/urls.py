e39f9d87e03c610be01ef3fe99eb4fa8cb51e09f
ayanaaza <aanaaza25@gmail.com>
2024-01-09 18:17:02 +0600

    first commit

---
+++ mytodo/tasks/urls.py
@@ -0,0 +1,10 @@
+# tasks/urls.py
+from django.urls import path
+from .views import task_list, add_task, toggle_task, delete_task
+
+urlpatterns = [
+    path('', task_list, name='task_list'),
+    path('add/', add_task, name='add_task'),
+    path('toggle/<int:task_id>/', toggle_task, name='toggle_task'),
+    path('delete/<int:task_id>/', delete_task, name='delete_task'),
+]
---
+++ mytodo/tasks/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.delete()
+    return redirect('task_list')
